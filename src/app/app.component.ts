373b242e6d7c63f2fe93ee54e31716b9f1785e8f
k1h1a1n <affykhan789678@gmail.com>
2022-01-29 21:36:43 +0530

    figma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'figma';
+  title = 'Shopping';
+col_Selected(){
+  var collection = document.querySelector<HTMLElement>('.collection')!;
+  collection.style.opacity = '1';
+}
+col_Left(){
+  var collection = document.querySelector<HTMLElement>('.collection')!;
+    collection.style.opacity = '0.6';
+}        
+
+about_Selected(){
+  var about = document.querySelector<HTMLElement>('.about')!;
+  about.style.opacity = '1';
+}
+about_Left(){
+  var about = document.querySelector<HTMLElement>('.about')!;
+  about.style.opacity = '0.6';
+}
+
+shop_Selected(){
+  var shop = document.querySelector<HTMLElement>('.shop')!;
+  shop.style.opacity = '1';
+}
+shop_Left(){
+  var shop = document.querySelector<HTMLElement>('.shop')!;
+  shop.style.opacity = '0.6';
+}
+
+blog_Selected(){
+  var blog = document.querySelector<HTMLElement>('.blog')!;
+  blog.style.opacity = '1';
+}
+blog_Left(){
+  var blog = document.querySelector<HTMLElement>('.blog')!;
+  blog.style.opacity = '0.6';  
+}
+
+review_Selected(){
+  var review = document.querySelector<HTMLElement>('.review')!;
+  review.style.opacity = '1';
+}
+review_Left(){
+  var review = document.querySelector<HTMLElement>('.review')!;
+  review.style.opacity = '0.6';  
+}
 }
